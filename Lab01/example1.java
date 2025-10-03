f3a0097bf26701ca6f2548c0e1b83ccb54981708
nmtuan2110 <bestt21102006@gmail.com>
2025-10-03 17:11:12 +0700

    update

--- Lab01/example1.java
+++
@@ -1,8 +0,0 @@
-package week4;
-
-public class example1 {
-    public static void main(String[] args) {
-        System.out.println("Xin chao \n cac ban");
-        System.out.println("Hello \t world");
-    }
-}
