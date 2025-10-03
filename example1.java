1daf280e828bed9fddd5e40d1a0a73aed74bf13b
nmtuan2110 <bestt21102006@gmail.com>
2025-10-03 16:02:51 +0700

    add file java

---
+++ example1.java
@@ -0,0 +1,8 @@
+package week4;
+
+public class example1 {
+    public static void main(String[] args) {
+        System.out.println("Xin chao \n cac ban");
+        System.out.println("Hello \t world");
+    }
+}
