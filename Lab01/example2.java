744a05451e00ec6b3a844536d5e49591fd490abc
nmtuan2110 <bestt21102006@gmail.com>
2025-10-03 16:14:55 +0700

    update

---
+++ Lab01/example2.java
@@ -0,0 +1,11 @@
+package week4;
+
+import javax.swing.JOptionPane;
+
+public class example2 {
+ public static void main(String[] args) {
+     JOptionPane.showMessageDialog(null, "Hello world! How are you?");
+     System.exit(0);
+ }
+}
+
