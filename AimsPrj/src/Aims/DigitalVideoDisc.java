8a7bbe7620ccdf45fdac39086112f9096ee1533c
nmtuan2110 <bestt21102006@gmail.com>
2025-10-31 12:07:19 +0700

    add class and instance attribute

--- AimsPrj/src/Aims/DigitalVideoDisc.java
+++ AimsPrj/src/Aims/DigitalVideoDisc.java
@@ -24,12 +24,16 @@ public class DigitalVideoDisc {
 	public DigitalVideoDisc(String title) {
 		super();
 		this.title = title;
+		nbDigitalVideoDiscs++;
+		this.id = nbDigitalVideoDiscs;
 	}
 	public DigitalVideoDisc(String title, String category, float cost) {
 		super();
 		this.title = title;
 		this.category = category;
 		this.cost = cost;
+		nbDigitalVideoDiscs++;
+		this.id = nbDigitalVideoDiscs;
 	}
 	public DigitalVideoDisc(String title, String category, String director, float cost) {
 		super();
@@ -37,6 +41,8 @@ public class DigitalVideoDisc {
 		this.category = category;
 		this.director = director;
 		this.cost = cost;
+		nbDigitalVideoDiscs++;
+		this.id = nbDigitalVideoDiscs;
 	}
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
 		super();
@@ -45,8 +51,12 @@ public class DigitalVideoDisc {
 		this.director = director;
 		this.length = length;
 		this.cost = cost;
+		nbDigitalVideoDiscs++;
+		this.id = nbDigitalVideoDiscs;
 	}
 	public void setTitle(String title) {
 		this.title = title;
 	}
-}
+	private static int nbDigitalVideoDiscs=0;
+	private int id;
+	}
