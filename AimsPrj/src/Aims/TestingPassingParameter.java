25cbf0c326e26bf61d2ef5059991d2d29eccca7c
nmtuan2110 <bestt21102006@gmail.com>
2025-10-31 11:51:19 +0700

    try debugging

--- AimsPrj/src/Aims/TestingPassingParameter.java
+++ AimsPrj/src/Aims/TestingPassingParameter.java
@@ -1,8 +1,6 @@
 package Aims;
 public class TestingPassingParameter {
-//swap method before being fixed
-    /*public static void main(String[] args) {
-        // TODO Auto-generated method stub
+	public static void main(String[] args) {
         DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
         DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
 
@@ -24,27 +22,5 @@ public class TestingPassingParameter {
         String oldTitle = dvd.getTitle();
         dvd.setTitle(title);
         dvd = new DigitalVideoDisc(oldTitle);
-    }*/
-
-	//after fixed
-	public static void main(String[] args) {
-	    DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
-	    DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
-	
-	    DigitalVideoDisc[] dvdList = {jungleDVD, cinderellaDVD};
-	
-	    swap(dvdList, 0, 1);
-	
-	    jungleDVD = dvdList[0];
-	    cinderellaDVD = dvdList[1];
-	
-	    System.out.println("jungleDVD title: " + jungleDVD.getTitle());
-	    System.out.println("cinderellaDVD title: " + cinderellaDVD.getTitle());
-	}
-	
-	public static void swap(DigitalVideoDisc[] arr, int i, int j) {
-	    DigitalVideoDisc tmp = arr[i];
-	    arr[i] = arr[j];
-	    arr[j] = tmp;
-	}
+    }
 }
